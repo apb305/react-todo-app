f58a716952312093396044c9a5ba5227a88e7eba
Anthony Bernard <tony@anthonys-mbp.home>
2019-07-21 23:28:26 -0400

    minor changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,17 @@ function Header() {
   return (
     <div>
       <p className="my-2 text-center font-weight-bold">
-        A To-Do Application written by Anthony Bernard. See code for this project{" "}
+        This is a{" "}
+        <a href="https://reactjs.org/" target="noopener noreferrer">
+          REACT JS
+        </a>{" "}
+        To-Do App written by Anthony Bernard. See the code for this project{" "}
         <a
           href="https://github.com/apb305/react-todo-app"
           target="noopener noreferrer"
         >
           here.
-        </a>{" "}
-        This project was built with the{" "}
-        <a href="https://reactjs.org/" target="noopener noreferrer">
-          REACT JS
-        </a>{" "}
-        library.
+        </a>
       </p>
     </div>
   );
